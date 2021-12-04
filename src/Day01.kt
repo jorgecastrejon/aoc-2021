0b9521b8935e9c34ae3000561680b1ea0e8ac75c
jorge castrejon <jorge.castrejon.anguiano@gmail.com>
2021-12-04 14:25:38 +0100

    Completing Day01 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,11 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<Int>): Int =
+        input.zipWithNext().count { (prev , next) -> prev < next }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<Int>): Int =
+        input.windowed(3).zipWithNext().count { (prev , next) -> prev.sum() < next.sum() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(part1(input))
     println(part2(input))
 }
