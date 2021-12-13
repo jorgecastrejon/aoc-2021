e9cc74710eb838073378e43190268acfd200f1f2
jorge castrejon <jorge.castrejon.anguiano@gmail.com>
2021-12-13 18:32:41 +0100

    little refactor

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,32 +1,15 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val separator = input.indexOf("")
-        val table: List<BoardPoint> = input.take(separator).map { position ->
-            position.split(",").let { (x, y) -> x.toInt() to y.toInt() }
-        }
-        val instructions: List<FoldInstruction> = input.takeLast(input.size - (separator + 1)).map { instruction ->
-            instruction.split(" ")
-                .let { (_, _, value) -> value.split("=").let { (axis, number) -> axis to number.toInt() } }
-        }
-
-        return table.foldWithInstruction(instructions.first()).count()
-    }
+    fun part1(input: List<String>): Int =
+        parsePaper(input).foldWithInstruction(parseInstructions(input).first()).count()
 
     fun part2(input: List<String>): Int {
-        val separator = input.indexOf("")
-        val table: List<BoardPoint> = input.take(separator).map { position ->
-            position.split(",").let { (x, y) -> x.toInt() to y.toInt() }
+        val paper = parseInstructions(input).fold(parsePaper(input)) { paper, instruction ->
+            paper.foldWithInstruction(instruction)
         }
-        val instructions: List<FoldInstruction> = input.takeLast(input.size - (separator + 1)).map { instruction ->
-            instruction.split(" ")
-                .let { (_, _, value) -> value.split("=").let { (axis, number) -> axis to number.toInt() } }
-        }
-        val foldedPaper = instructions.fold(table) { board, instruction -> board.foldWithInstruction(instruction) }
-        printTable(foldedPaper)
+        printPaper(paper)
 
-        return foldedPaper.count()
-
+        return paper.count()
     }
 
     val input = readInput("Day13")
@@ -34,23 +17,7 @@ fun main() {
     println(part2(input))
 }
 
-private fun printTable(table: List<BoardPoint>) {
-    val paper = table.toSet()
-
-    for (y in paper.minOf { point -> point.py }..paper.maxOf { point -> point.py }) {
-        println("")
-        for (x in paper.minOf { point -> point.px }..paper.maxOf { point -> point.px }) {
-            if (x to y in paper) {
-                print("#")
-            } else {
-                print(" ")
-            }
-        }
-    }
-    println("")
-}
-
-private fun List<BoardPoint>.foldWithInstruction(instruction: FoldInstruction): List<BoardPoint> =
+private fun Paper.foldWithInstruction(instruction: FoldInstruction): Paper =
     map { point -> fold(point, instruction) }
         .filter {
             if (instruction.axis == "y") {
@@ -61,26 +28,49 @@ private fun List<BoardPoint>.foldWithInstruction(instruction: FoldInstruction): 
         }
         .distinct()
 
-private fun fold(point: BoardPoint, instructions: FoldInstruction): BoardPoint {
-    return if (instructions.axis == "x") {
-        if (point.px > instructions.value) {
+private fun fold(point: PaperPoint, instructions: FoldInstruction): PaperPoint =
+    when {
+        instructions.axis == "x" && point.px > instructions.value -> {
             (instructions.value - (point.px - instructions.value)) to point.py
-        } else {
-            point
         }
-    } else {
-        if (point.py > instructions.value) {
+        instructions.axis == "y" && point.py > instructions.value -> {
             point.px to (instructions.value - (point.py - instructions.value))
-        } else {
-            point
         }
+        else -> point
+    }
+
+private fun printPaper(originalPaper: Paper) {
+    val paper = originalPaper.toSet()
+
+    for (y in paper.minOf { point -> point.py }..paper.maxOf { point -> point.py }) {
+        println("")
+        for (x in paper.minOf { point -> point.px }..paper.maxOf { point -> point.px }) {
+            if (x to y in paper) print("#") else print(" ")
+        }
+    }
+    println("")
+}
+
+private fun parsePaper(input: List<String>): Paper {
+    val count = input.indexOf("")
+
+    return input.take(count).map { point -> point.split(",").let { (x, y) -> x.toInt() to y.toInt() } }
+}
+
+private fun parseInstructions(input: List<String>): List<FoldInstruction> {
+    val count = input.indexOf("")
+
+    return input.takeLast(input.size - (count + 1)).map { instruction ->
+        instruction.split(" ")
+            .let { (_, _, value) -> value.split("=").let { (axis, number) -> axis to number.toInt() } }
     }
 }
 
-typealias BoardPoint = Pair<Int, Int>
+typealias Paper = List<PaperPoint>
+typealias PaperPoint = Pair<Int, Int>
 
-val BoardPoint.px get() = first
-val BoardPoint.py get() = second
+val PaperPoint.px get() = first
+val PaperPoint.py get() = second
 typealias FoldInstruction = Pair<String, Int>
 
 val FoldInstruction.axis get() = first
